a47971e9f2669e0ce28a9fefe0015afffa916f4a
Fireye <codekai16@gmail.com>
2025-11-06 16:42:26 -0700

    Build raycast init

--- Scripts/HandTarget.cs
+++ Scripts/HandTarget.cs
@@ -3,8 +3,11 @@ using System;
 
 public partial class HandTarget : Marker3D {
     public GridMap grid;
+    public PlacementCast placementCast;
+
     // Called when the node enters the scene tree for the first time.
     public override void _Ready() {
+        placementCast = GetNode<PlacementCast>("%PlacementCast");
 
         GameState.Instance.GridSet += (val) => setGrid(val);
     }
@@ -14,7 +17,25 @@ public partial class HandTarget : Marker3D {
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(double delta) {}
 
-    public Vector3I getGridLoc() {
+    public Vector3I getRawGridLoc() {
         return grid.LocalToMap(grid.ToLocal(GlobalPosition));
+    }
+
+    public Vector3I getGridLoc() {
+        Vector3I pointerLoc = getRawGridLoc();
+
+        if (grid.GetCellItem(pointerLoc) == -1) {
+            return pointerLoc;
+        }
+        // Something's there, adapt
+        // TODO: raycast
+
+        // get local positive z of self (points to player)
+        // convert to Global vector3
+        // get raw x y and z of aformentioned global vector3
+        // for positive x y and z's math.max the corresponding axis, else
+        // math.min, store as target ideally, this limits the placeable faces to
+        // the 3 that face towards the player next, add one to the axis closest
+        // to its target.
     }
 }
